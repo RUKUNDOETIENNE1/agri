7c46edac71dc9fae61c205175464047cb8aec581
RUKUNDOETIENNE1 <agripal.rw@gmail.com>
2025-07-13 23:37:37 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@
 
 const API_CONFIG = {
   OPEN_WEATHER: {
-    KEY: "e784c84bbfdb662f5831fbb9c4116e7d", // ← Pasted from OpenWeatherMap API key
+    KEY: "", // ← Pasted from OpenWeatherMap API key
     BASE_URL: "https://api.openweathermap.org/data/2.5",
     UNITS: "metric", // Celsius
     EXCLUDE: "minutely,hourly", // Only need daily
